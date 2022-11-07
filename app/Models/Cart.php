b8c1bac20d50deb7043dca0e1610ad072e7013c5
dynamic2code <dynamicdesingns@gmail.com>
2022-11-07 22:50:22 +0300

    items

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+    protected $table = 'items';    
     //store items to cart
+
+   
     
     //retrive the no of items from cart
 }
